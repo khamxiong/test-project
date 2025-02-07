7e275085690eff4934cef2ec1c36a76b6ac749d3
khamxiong <khamxiongvaxa@gmail.com>
2025-02-07 11:01:29 +0700

    edit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,14 @@ const Footer = () => {
   return (
     <footer>
        <div className='w-full h-[50px] bg-slate-600'>
-          
+          dfsfsfsdfsf
+
+
+          sfsafasdfsafsa
+
+
+          s
+          fsafsafsadfasfasf
        </div>
     </footer>
   )
