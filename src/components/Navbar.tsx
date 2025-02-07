53d47d6a0af6ed1295851134e19153e3933025c1
khamxiong <khamxiongvaxa@gmail.com>
2025-02-07 10:46:15 +0700

    edit navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,6 +7,21 @@ const Navbar = () => {
             <Link to="/">Home</Link>
             <Link to="/about">About</Link>
             <Link to="/blogs">Blogs</Link>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/blogs">Blogs</Link>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/blogs">Blogs</Link>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/blogs">Blogs</Link>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/blogs">Blogs</Link>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/blogs">Blogs</Link>
          </nav>
     </header>
   )
